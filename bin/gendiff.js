7b119c9e613155a4212db5e9fc218464ff61a3d9
Farid Aliev <faridaliev@MacBook-Pro-Farid.local>
2024-06-18 14:48:34 +0300

    .

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,12 +6,12 @@ import gendiff from '../src/index.js';
 const program = new Command();
 
 program
- .name('gendiff')
- .description('Compares two configeration files and shows a difference.')
- .version('1.0.0')
- .option('-f, --format [type]', 'output format')
- .arguments('<filepath1> <filepath2>')
- .action((filepath1, filepath2) => {
+  .name('gendiff')
+  .description('Compares two configeration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
     console.log(gendiff(filepath1, filepath2));
- });
- program.parse();
+  });
+program.parse();
--- index.js
+++
@@ -1,3 +0,0 @@
-import sum from "./src/index.js";
-
-export default sum;
