1b0a9f8016062a5a6cc54a0efe46f07da047c472
Farid Aliev <faridaliev@MacBook-Pro-Farid.local>
2024-07-03 16:15:54 +0300

    .

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,13 @@
 import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import genDiff from '../index.js';
-import getFixturePath from '../src/helpers.js';
+
+const getFixturePath = (filename) => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  return path.join(__dirname, '..', '__fixtures__', filename);
+};
 
 const data = [
   {
